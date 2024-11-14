829b36e67df0df324270b13146c8360436365cf8
Anton <anton@MacBook-Pro-Anton.local>
2024-11-14 14:04:44 +0200

    task

---
+++ app/tasks.py
@@ -0,0 +1,20 @@
+tasks = [
+    {
+        {"id": 1},
+        {"name", "to do homework"},
+        {"dependency": 1},
+        {"deadline": "16.11.2024"}
+    },
+    {
+        {"id": 2},
+        {"name", "to do algebra homework"},
+        {"dependency": 1},
+        {"deadline": "15.11.2024"}
+    },
+    {
+        {"id": 3},
+        {"name", "to do python homework"},
+        {"dependency": 1},
+        {"deadline": "14.11.2024"}
+    }
+]
